e4503750ec61c963867a86e3887804481f0b80d3
LiamTubbs <liamjackson785@gmail.com>
2025-03-28 09:05:23 -0500

    Create datatypes.php

---
+++ datatypes.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP Datatypes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<?php
+
+    //String
+    $name = "I enjoy coding";
+
+    //Interger
+    $name= 15;
+
+    //float 
+    $name = 15.15;
+
+    //Boolean
+    true = 1
+    false = 0
+
+    //Array
+    $names = array("Liam", "Jackson", "Tubbs");
+    echo $names ['2'];
+
+?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
